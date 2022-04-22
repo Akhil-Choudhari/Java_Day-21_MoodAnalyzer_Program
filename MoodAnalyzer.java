b7e327d4a7e7c139fab96b5d284f144ba0bb4e7f
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-04-22 23:43:56 +0530

    Mood Analyzer program

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,32 +1,24 @@
 package MoodAnalyzerProgram;
 
-import MoodAnalyzerProgram.MoodAnalyzerException.ExceptionType;
-
 public class MoodAnalyzer {
-
+	
 	String message;
-
+	
 	public MoodAnalyzer() {
-
+		
 	}
-
-	public MoodAnalyzer(String message) {
+	
+	public MoodAnalyzer(String message){
 		this.message = message;
 	}
 
-	public String analyzeMood() throws MoodAnalyzerException {
-
-		try {
-			if (message.length() == 0) {
-				throw new MoodAnalyzerException(ExceptionType.ENTERED_EMPTY, "Enter Proper Message. EMPTY Not Allowed");
-			}
-			if (this.message.contains("Sad")) {
-				return "SAD";
-			} else {
-				return "HAPPY";
-			}
-		} catch (NullPointerException e) {
-			throw new MoodAnalyzerException(ExceptionType.ENTERED_NULL, "Enter Proper Message. NULL Not Allowed");
+	public String analyzeMood() {
+		
+		if(this.message.contains("Sad")) {
+			return "SAD";
+		}
+		else {
+			return "HAPPY";
 		}
 	}
 }
